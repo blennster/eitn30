9b23c642e668cc11c40a33c07b852935f749ed9c
blennster <emil.blennow99@gmail.com>
2023-01-27 14:30:28 +0100

    tcp connections work

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{
     env,
     io::{Read, Write},
-    sync::{Arc, Mutex},
     thread::{self, sleep},
     time::Duration,
 };
@@ -12,9 +11,10 @@ extern crate tun;
 
 mod virtual_interface;
 
-const MTU: usize = 200;
+const MTU: usize = 500;
+const RESET_BUF: [u8; 3] = [255, 255, 255];
 
-macro_rules! println {
+macro_rules! debug_println {
     ($($rest:tt)*) => {
         if std::env::var("DEBUG").is_ok() {
             std::println!($($rest)*);
@@ -57,105 +57,114 @@ fn main() {
 
     let dev = tun::create(&config).unwrap();
 
-    let config = RXConfig {
+    let config_rx = RXConfig {
         channel: 110,
         pa_level: PALevel::Low,
         pipe0_address: addr,
         data_rate: DataRate::R2Mbps,
         ..Default::default()
     };
-    let mut nrf_rx = NRF24L01::new(17, 0, 0).unwrap();
-    nrf_rx.configure(&OperatingMode::RX(config)).unwrap();
-    nrf_rx.listen().unwrap();
-    let config = TXConfig {
+    let config_tx = TXConfig {
         channel: 110,
         pa_level: PALevel::Low,
         pipe0_address: other_addr,
         max_retries: 7,
-        retry_delay: 2,
+        retry_delay: 1,
         data_rate: DataRate::R2Mbps,
     };
+
+    assert!(
+        config_rx.data_rate == DataRate::R2Mbps
+            && config_rx.data_rate == config_tx.data_rate
+            && config_rx.channel == config_tx.channel
+            && config_rx.pipe0_address != config_tx.pipe0_address
+    );
+
+    let mut nrf_rx = NRF24L01::new(17, 0, 0).unwrap();
+    nrf_rx.configure(&OperatingMode::RX(config_rx)).unwrap();
+    nrf_rx.flush_input().unwrap();
+    nrf_rx.listen().unwrap();
+
     let mut nrf_tx = NRF24L01::new(27, 1, 0).unwrap();
-    nrf_tx.configure(&OperatingMode::TX(config)).unwrap();
+    nrf_tx.configure(&OperatingMode::TX(config_tx)).unwrap();
     nrf_tx.flush_output().unwrap();
 
     tun(dev, nrf_rx, nrf_tx, delay);
-
-    // match args[1].as_str() {
-    //     "rx" => tun_rx(&mut dev, &mut nrf_rx, &mut nrf_tx),
-    //     "tx" => tun_tx(&mut dev, &mut nrf_rx, &mut nrf_tx),
-    //     _ => panic!(),
-    // }
 }
 
-fn tun(mut dev: tun::platform::Device, mut nrf_rx: NRF24L01, mut nrf_tx: NRF24L01, delay: u64) {
-    dev.set_nonblock().unwrap();
-    let tun_arc = Arc::new(Mutex::new(dev));
+fn tun(dev: tun::platform::Device, mut nrf_rx: NRF24L01, mut nrf_tx: NRF24L01, delay: u64) {
+    let (mut reader, mut writer) = dev.split();
 
     // RX loop
-    let tun = Arc::clone(&tun_arc);
-    thread::spawn(move || {
+    let rx = thread::spawn(move || {
         println!("rx thread started!");
-        let mut buf = [0u8; MTU];
-        let mut offset = 0;
+        let mut buf = [0u8; MTU * 2];
+        let mut end = 0;
+        let mut reset_buf = false;
+
         loop {
             sleep(Duration::from_micros(delay));
             if nrf_rx.data_available().unwrap() {
                 let n = nrf_rx
                     .read_all(|packet| {
-                        for (i, byte) in packet.iter().enumerate() {
-                            if offset + i >= MTU {
-                                offset = 0;
-                                break;
-                            }
-                            buf[i + offset] = *byte;
+                        for byte in packet.iter() {
+                            buf[end] = *byte;
+                            end += 1;
                         }
-                        offset += packet.len();
                     })
-                    .unwrap();
-                if offset >= MTU {
-                    offset = 0;
-                    println!("buf is bigger than mtu, resetting");
-                    continue;
+                    .unwrap() as u32;
+
+                for packet in buf[0..end].chunks(32) {
+                    if packet.eq(&RESET_BUF) {
+                        reset_buf = true;
+                        continue;
+                    }
                 }
 
-                println!("{} packets received", n);
+                if reset_buf {
+                    end = 0;
+                    reset_buf = false;
+                    debug_println!("reset buf command received");
+                }
+
+                debug_println!("{} packets received", n);
                 // Make sure the packet is valid
-                if packet::ip::Packet::new(&buf[0..offset]).is_ok() {
-                    let mut tun = tun.lock().unwrap();
-                    let result = tun.write(&buf[0..offset]);
-                    drop(tun);
+                if end != 0 && packet::ip::Packet::new(&buf[0..end]).is_ok() {
+                    let result = writer.write(&buf[0..end]);
                     match result {
-                        Ok(n) => println!("{} bytes written to interface", n),
-                        Err(_) => println!("could not write to interface"),
+                        Ok(n) => debug_println!("{} bytes written to interface", n),
+                        Err(err) => debug_println!("{} error when writing to interface", err),
                     }
-                    offset = 0;
+                    end = 0;
                 }
             }
         }
     });
 
     // TX loop
-    let tun = Arc::clone(&tun_arc);
-    thread::spawn(move || {
+    let tx = thread::spawn(move || {
         println!("tx thread started!");
         let mut buf = [0u8; MTU];
         loop {
             sleep(Duration::from_micros(delay));
-            let mut tun = tun.lock().unwrap();
-            let read_result = tun.read(&mut buf);
-            drop(tun);
+            let read_result = reader.read(&mut buf);
             if let Ok(n) = read_result {
                 if n == 0 {
                     continue;
                 }
 
                 let pkt = &buf[0..n];
+                if packet::ip::Packet::new(pkt).is_err() {
+                    debug_println!("read an invalid packet from tun, dropping");
+                    continue;
+                }
+
                 let mut chunks = vec![];
                 let mut queue = vec![];
-                for (i, chunk) in pkt.chunks(32).enumerate() {
+
+                for chunk in pkt.chunks(32) {
                     queue.push(chunk);
-                    if (i + 1) % 3 == 0 {
+                    if queue.len() == 2 {
                         chunks.push(queue.clone());
                         queue.clear();
                     }
@@ -167,18 +176,28 @@ fn tun(mut dev: tun::platform::Device, mut nrf_rx: NRF24L01, mut nrf_tx: NRF24L0
                 for queue in chunks {
                     for pkt in queue {
                         nrf_tx.push(0, pkt).unwrap();
+                        debug_println!("queuing {:?}", pkt);
                     }
                     match nrf_tx.send() {
-                        Ok(retries) => println!("Message sent, {} retries needed", retries),
+                        Ok(retries) => debug_println!("message sent, {} retries needed", retries),
                         Err(err) => {
-                            println!("Destination unreachable: {:?}", err);
-                            nrf_tx.flush_output().unwrap()
+                            debug_println!("destination unreachable: {:?}", err);
+                            nrf_tx.flush_output().unwrap();
+                            // The packet cannot be sent in any meaningful way, abort
+                            break;
                         }
                     };
+                    sleep(Duration::from_micros(delay / 2));
                 }
+
+                // Send reset buf command, may fail but not really important
+                nrf_tx.push(0, &RESET_BUF).ok();
+                nrf_tx.send().ok();
+                nrf_tx.flush_output().unwrap();
             }
         }
-    })
-    .join()
-    .unwrap();
+    });
+
+    rx.join().unwrap();
+    tx.join().unwrap();
 }
